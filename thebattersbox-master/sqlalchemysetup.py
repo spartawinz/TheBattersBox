c7fc0e5fac4784c88201c527665025283e6feba5
spartawinz <tpratt1994@gmail.com>
2017-09-21 13:50:33 -0500

    blank project
    uploaded the wrong one cause reasons

--- thebattersbox-master/sqlalchemysetup.py
+++
@@ -1,5 +0,0 @@
-import sqlalchemy
-from sqlalchemy import create_engine
-
-def function initSqlAlchemy():
-    engine = create_engine('mysql+mysqldb://admin:Group2017@localhost/thebattersbox', echo=true);
